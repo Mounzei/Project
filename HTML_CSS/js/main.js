c956b861e6ec2a131ec5fd047cc20495a4fc6d3b
Mounzei <78475357+Mounzei@users.noreply.github.com>
2021-04-14 15:22:18 +0200

    Add files via upload
    Projet HTML/CSS

---
+++ HTML_CSS/js/main.js
@@ -0,0 +1,4 @@
+alert("Bienvenue à vous!")
+let rep = prompt("Si vous souhaitez envoyé votre cadeau taper 1 ou si vous souhaitez venir le déposer taper 2. Vous recevrez ensuite des instructions par mail conformément à votre choix.")
+
+
