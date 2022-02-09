a1baa7a3b0315b9c97c693eabf6d09dbf8da7d0e
Mounzei <78475357+Mounzei@users.noreply.github.com>
2022-02-09 14:36:15 +0100

    Delete Casper V7 directory

--- Casper V7/aboutUs.php
+++
@@ -1,6 +0,0 @@
-<?php
-$template = "aboutUs";
-$title = "Qui sont nous ?";
-require 'Functions/bdd.php';
-require 'Functions/functions.php';
-include 'index.phtml';
--- Casper V7/accueil.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$template = "accueil";
-$title = "ACCUEIL";
-require 'Functions/bdd.php';
-require 'Functions/functions.php';
-include 'index.phtml';
