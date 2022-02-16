f6e26ebd8284012848d6c0b992d83fd1fefefa24
Mounzei <78475357+Mounzei@users.noreply.github.com>
2022-02-16 19:03:55 +0100

    Update bdd.php

--- Site de vente de bijoux/Functions/bdd.php
+++ Site de vente de bijoux/Functions/bdd.php
@@ -1,28 +1,16 @@
 <?php
 
-$hn = 'localhost';
-$un = 'root';
-$pw = '';
-$dbn = 'casper';
-
-// msg erreur
-
-function mysql_fatal_error()
-{
-    echo <<< _END
-    Nous sommes désolés mais il n'est pas possible d'effectuer la tâche demandée. L'erreur rencontré est la suivante: 
-    <p>Erreur fatale</p>
-    Cliquez sur le bouton précédent de votre navigateur et réessayer. Si le problèmes persistent ou que vous en rencontriez de nouveux
-    communiquez avec <a href="mailto:mounzei@outlook.fr">notre administrateur</a>. Merci.
-    _END;
-};
+$hostname = '';
+$username = '';
+$password = '';
+$databasename = '';
 
 // creation loien base de donnée
 
 $pdo = new PDO(
-    "mysql:host=$hn;port=3306;dbname=$dbn",
-    "$un",
-    "$pw"
+    "mysql:host=$hostname;port=3306;dbname=$databasename",
+    "$username",
+    "$password"
 );
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $pdo->exec('SET NAMES UTF8');
